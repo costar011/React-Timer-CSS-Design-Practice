6200a611c25c26fd827a36878a6c4346248c2641
YERIM <dnaer456@gmail.com>
2020-06-10 21:40:50 +0900

    CSS Design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let autoTime = null;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timeSecond: 0,
+      isStart: false,
+      records: [],
+    };
+  }
+  render() {
+    const { timeSecond, records } = this.state;
+    return (
+      <div className="timerBox">
+        <div className="timerBox__time">
+          <span>{timeSecond}</span>
+        </div>
+
+        <div className="timerBox__record">
+          <ul>
+            {records.map((re, index) => {
+              return <li key={index}>{re}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="timerBox__btn">
+          <input type="button" value="START" onClick={this._startTimer}></input>
+          <input type="button" value="STOP" onClick={this._stopTimer}></input>
+          <input type="button" value="RECODE" onClick={this._recordBtn}></input>
+          <input type="button" value="INIT" onClick={this._recordInit}></input>
+        </div>
+      </div>
+    );
+  }
+
+  _startTimer = () => {
+    const { isStart } = this.state;
+    if (!isStart) {
+      this.setState({
+        isStart: true,
+      });
+
+      autoTime = setInterval(() => {
+        this.setState({
+          timeSecond: this.state.timeSecond + 1,
+        });
+      }, 1000);
+    } else {
+      alert("이미 타이머가 작동중");
+    }
+  };
+
+  _stopTimer = () => {
+    this.setState({
+      isStart: false,
+    });
+
+    clearInterval(autoTime);
+    this.setState({
+      timeSecond: 0,
+    });
+  };
+
+  _recordBtn = () => {
+    const { isStart, records } = this.state;
+
+    if (isStart) {
+      records.push(this.state.timeSecond);
+    } else {
+      return;
+    }
+  };
+
+  _recordInit = () => {
+    this.setState({
+      records: [],
+    });
+  };
 }
-
 export default App;
